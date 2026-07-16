bc167d76f659872fccda29e80f0e4196ceb1a30e
Sri-Lakshmi-Sripadam <srilakshmisripadam777@gmail.com>
2026-07-16 22:30:43 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+temp = n
+digits = len(str(n))
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit ** digits
+    temp //= 10
+
+if total == n:
+    print("Armstrong")
+else:
+    print("Not Armstrong")
